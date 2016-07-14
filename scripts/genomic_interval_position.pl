2b662eeb73d67a82d90aa73b4063c0d245b6a4b9
Francois Sabot <francois.sabot@ird.fr>
2016-07-14 12:06:21 +0200

    adding licenses

--- scripts/genomic_interval_position.pl
+++ scripts/genomic_interval_position.pl
@@ -2,12 +2,33 @@
 
 #!/usr/bin/perl
 
-# title: Split_genome
-# Author: Gustave
-# Goal: Split lines into genomic intervals (blocks)
-# Retrieve the file name and open the file 
-#  First associate with the file, a descriptor
-#  used FIC
+
+###################################################################################################################################
+#
+# Copyright 2016 IRD-FAST/DASSA
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/> or
+# write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# You should have received a copy of the CeCILL-C license with this program.
+#If not see <http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.txt>
+#
+# Intellectual property belongs to IRD and FAST/DASSA
+# Version 1 written by Gustave Djedatin and Francois Sabot
+####################################################################################################################################
 
 
 use strict;
